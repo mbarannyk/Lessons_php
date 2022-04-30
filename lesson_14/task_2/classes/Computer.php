403c711bee1bdb6227d33863dff4a0393186d981
mbarannyk <maryano4ka2008@ukr.net>
2022-04-30 12:29:14 +0300

    add lesson 14

--- lesson_14/task_2/classes/Computer.php
+++ lesson_14/task_2/classes/Computer.php
@@ -3,5 +3,32 @@
 $conn = new PDO("mysql:host=localhost; dbname=Cities", "root", "root");
 $word = $_GET['city'];
 $letter = mb_substr($word, -1);
-$result = $conn->query("SELECT name FROM Ukrainian_Cities WHERE name LIKE '$letter%'");
-echo $result->fetch()['name'];
+$result = $conn->query("SELECT name FROM Ukrainian_Cities WHERE name LIKE '$letter%' ORDER BY RAND()");
+$BotCity = $result->fetch()['name'];
+echo $BotCity;
+
+function contains($cities, $BotCity) {
+        for ($i = 0; $i <= count($cities); $i++) {
+            if ($cities[$i] == $BotCity) {
+                return $result;
+            } else {
+            return $BotCity = $result->fetch()['name'];
+            }
+     }
+}
+
+
+
+if (isset($_COOKIE['BotCity'])) {
+    $cities[] = $_COOKIE['BotCity'];
+    setcookie($BotCity, serialize($cities), time()+900);
+       
+    }  else {
+        $cities[] = $BotCity;
+        setcookie($BotCity, serialize($cities), time()+900);
+        
+}  
+
+
+
+$letter1 = mb_substr($BotCity, -1);
